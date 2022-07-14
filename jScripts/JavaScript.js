cbc92ff721465b921a4be6b3b7c06670d53c7cf4
barshitrit17 <barshitrit17@gmail.com>
2022-07-15 02:14:49 +0300

    newProject
    OurNewProject

--- jScripts/JavaScript.js
+++ jScripts/JavaScript.js
@@ -1,24 +1,36 @@
 ﻿(function ()
 {
     // how to play click
-    var howToPlayElement = window.document.getElementsByClassName("howToPlay")[0];
-    howToPlayElement.onclick = function () {
-        window.document.getElementsByClassName("howToPlayImage")[0].style.display = "block";
-        window.document.getElementsByClassName("closeButton")[0].style.display = "block";
+    var HowToPlayElement = window.document.getElementsByClassName("howToPlay")[0];
+    HowToPlayElement.onclick = function () {
+        window.document.getElementsByClassName("FinalHowToPlayAnimationWrapper")[0].style.display = "block";
     }
 
     // about click
     var aboutElement = window.document.getElementsByClassName("about")[0];
     aboutElement.onclick = function () {
-        window.document.getElementsByClassName("aboutImage")[0].style.display = "block";
-        window.document.getElementsByClassName("closeButton")[0].style.display = "block";
+        window.document.getElementsByClassName("aboutImageWrapper")[0].style.display = "block";
     }
 
     // close click
-    var closeButtonElement = window.document.getElementsByClassName("closeButton")[0];
-    closeButtonElement.onclick = function () {
-        window.document.getElementsByClassName("howToPlayImage")[0].style.display = "none";
-        window.document.getElementsByClassName("aboutImage")[0].style.display = "none";
-        window.document.getElementsByClassName("closeButton")[0].style.display = "none";
+    var closeButtonElements = window.document.getElementsByClassName("closeButton");
+    for (var i = 0; i < closeButtonElements.length; i++) {
+        closeButtonElements[i].onclick = function () {
+            window.document.getElementsByClassName("FinalHowToPlayAnimationWrapper")[0].style.display = "none";
+            window.document.getElementsByClassName("aboutImageWrapper")[0].style.display = "none";
+        }
+    }
+
+    // editor click
+    var editorElement = window.document.getElementsByClassName("editor")[0];
+    editorElement.onclick = function () {
+        var ourGame = window.document.getElementById("ourGame");
+        ourGame.src = "https://anittamali.github.io/WitchIsRightGame/witch_is_right.html";
+    }
+
+    var ourGame = window.document.getElementById("ourGame");
+    ourGame.onload = function () {
+        var onLoad = window.document.getElementsByClassName("onLoad")[0];
+        onLoad.style.display = "none";
     }
 })();
